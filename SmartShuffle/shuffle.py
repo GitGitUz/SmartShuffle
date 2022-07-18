e090a6dd50cd0295de5b6220ed09443407c7e0a7
GitGitUZ <uzairshaikh000@gmail.com>
2022-07-18 02:01:52 -0400

    Fixed grouping function, works as expected

--- SmartShuffle/shuffle.py
+++ SmartShuffle/shuffle.py
@@ -6,7 +6,21 @@ def groupPlaylistSongs(playlist):
     grouped_playlist = []
     key_func = lambda song:song['artists'][0]['name']
     for key, group in itertools.groupby(playlist, key_func):
-        grouped_playlist += list(group)
+        group_as_list = list(group)
+        print(f"ARTIST: {key} - {[s[1]['name'] for s in group_as_list]}")
+        grouped_playlist += [[s[1] for s in group_as_list]]
+    return grouped_playlist
+
+#testing version of grouping method
+def groupPlaylistSongsTEST(playlist):
+    print("\n", playlist, "\n")
+    grouped_playlist = []
+    key_func = lambda song:song[0]
+    for key, group in itertools.groupby(playlist, key_func):
+        group_as_list = list(group)
+        print(f"ARTIST: {key} - {[s[1] for s in group_as_list]}")
+        grouped_playlist += [[s[1] for s in group_as_list]]
+    print("\nGP: ", grouped_playlist)
     return grouped_playlist
 
 #a simple Fisher-Yates shuffle algorithm to be used within each category
@@ -27,9 +41,9 @@ def spotifyShuffle(playlist):
     for artist_songs in range(len(playlist)):
         items += fyShuffle(playlist[artist_songs])
         n = len(playlist[artist_songs])
-        io = random.uniform(0,1/n)
-        o = [random.uniform(-0.1/n, 0.1/n) for k in range(n)]
-        v += [k/n + io + o[k] for k in range(n)]
+        initialOffset = random.uniform(0,1/n)
+        randomOffset = [random.uniform(-0.1/n, 0.1/n) for k in range(n)]
+        v += [k/n + initialOffset + randomOffset[k] for k in range(n)]
     
     #sort items by positional vector
     shuffled = [s[1] for s in sorted(zip(v,items))]
@@ -38,4 +52,7 @@ def spotifyShuffle(playlist):
 
 playlist1 = [["Numb", "In The End", "Crawling", "Papercut"],["The Hills", "Starboy"],["Power", "Runaway", "Jail", "Jesus Walks", "Heartless"], ["Sicko Mode"]]
 playlist2 = [["LP1", "LP2", "LP3", "LP4"],["TW1", "TW2"],["KW1", "KW2", "KW3", "KW4", "KW5"], ["TS1"]]
-print(spotifyShuffle(playlist2))
+playlist3 = [["Travis Scott", "90210"],["Travis Scott", "Goosebumps"],["Linkin Park", "Numb"],["Linkin Park", "In The End"],["Linkin Park", "Crawling"],["The Weeknd", "Gasoline"],["The Weeknd", "Starboy"],["The Weeknd", "The Hills"]]
+groupPlaylistSongsTEST(playlist3)
+print()
+print(spotifyShuffle(groupPlaylistSongsTEST(playlist3)))
