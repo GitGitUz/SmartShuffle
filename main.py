56503cd00b43dcee97ed2fa78405114303f065e3
GitGitUZ <uzairshaikh000@gmail.com>
2022-08-02 18:38:43 -0400

    commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from website import create_app
 import os
 
 app = create_app()
-port = int(os.getenv('PORT'))
+# port = int(os.getenv('PORT'))
 
 if __name__ == '__main__':
-    app.run(port=port, debug=True)
+    app.run(debug=True)
